2bd10897e1727de504c67857b43b0d8730bebf72
dardanmat <dardan.berisha@stud.unifi.it>
2025-05-01 17:11:31 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Il Post</title>
+    <link rel="icon" href="https://placehold.co/50x50" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-white bg-white fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Il Mio Sito</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#servizi">Servizi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contatti">Contatti</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container mt-5 pt-5">
+        <h1>Il Post</h1>
+        <p>Benvenuto nel mio sito!</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+    <p>Somebody</p>
+
+    <!-- Bootstrap 5 JS Bundle (incluso Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
